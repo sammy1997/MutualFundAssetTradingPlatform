60f8342969e388e734e632be5213185dcd68dc3a
Sombuddha Chakravarty <somchakr@publicisgroupe.net>
2019-06-10 15:07:12 +0530

    Added http get/post example in React

---
+++ platform-ui/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <PostList></PostList>
+    </div>
+  );
+}
+
+export default App;
