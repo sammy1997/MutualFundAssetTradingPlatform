081d134ec83affc7de8f37a4b5e170ff15278687
erimawlong <ericmawlong@gmail.com>
2019-07-10 16:18:51 +0530

    Trading service tests added

--- Microservices/trading-service/src/test/java/io/tradingservice/tradingservice/controllerTests/ControllerTests.java
+++ Microservices/trading-service/src/test/java/io/tradingservice/tradingservice/controllerTests/ControllerTests.java
@@ -15,12 +15,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-
 import static javax.ws.rs.core.Response.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,40 +47,44 @@ public class ControllerTests {
 
     @Test
     public void successTest(){
+
         assert 1==1;
     }
 
     //Setting up dummy trade
     @Before
     public void setUp(){
+
         baseUrl = "http://localhost:" + randomServerPort;
         trade = ImmutableTrade.builder().fundNumber("1234").fundName("Hedge").avgNav((float)22)
                 .status("purchase").quantity((float)7).invManager("GS").setCycle(2).invCurr("INR")
                 .sAndPRating((float)23.2).moodysRating((float)12).build();
     }
 
-    //View trades end point
+    /*  //View trades end point
 
-//    @Test
-//    public void viewTradeEndpointTest() throws Exception{
-//        URI uri = new URI(baseUrl + "/view");
-//
-//        List<ImmutableTrade> trades = new ArrayList<>();
-//        trades.add(trade);
-//
-//        Mockito.when(userTradeService.getAllTrades(Mockito.anyString())).thenReturn(trades);
-//
-//        ResponseEntity<String> entity= this.restTemplate.getForEntity(uri, String.class);
-//
-//        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//
-//        String expected = "[{\"fundNumber\":\"1234\",\"fundName\":\"Hedge\",\"avgNav\":22.0,\"status\":\"purchase\",\"quantity\":7.0,\"invManager\":\"GS\",\"setCycle\":2,\"invCurr\":\"INR\",\"sAndPRating\":23.2,\"moodysRating\":12.0}]";
-//        assertThat(expected).isEqualTo(entity.getBody());
-//    }
+    @Test
+    public void viewTradeEndpointTest() throws Exception{
+
+        URI uri = new URI(baseUrl + "/view");
+
+        List<ImmutableTrade> trades = new ArrayList<>();
+        trades.add(trade);
+
+        Mockito.when(userTradeService.getAllTrades(Mockito.anyString())).thenReturn(trades);
+
+        ResponseEntity<String> entity= this.restTemplate.getForEntity(uri, String.class);
+
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        String expected = "[{\"fundNumber\":\"1234\",\"fundName\":\"Hedge\",\"avgNav\":22.0,\"status\":\"purchase\",\"quantity\":7.0,\"invManager\":\"GS\",\"setCycle\":2,\"invCurr\":\"INR\",\"sAndPRating\":23.2,\"moodysRating\":12.0}]";
+        assertThat(expected).isEqualTo(entity.getBody());
+    }
 
     //Exchange trades end point
-   /* @Test
+    @Test
     public void exchangeTradesEndpointTest() throws Exception{
+
         URI uri = new URI(baseUrl + "/exchange");
 
         List<TradeParser> t1 = new ArrayList<>();
@@ -102,6 +104,7 @@ public class ControllerTests {
     //Verify trades end point
     @Test
     public void verifyTradesEndpointTest() throws Exception{
+
         URI uri = new URI(baseUrl + "/verify");
 
         Mockito.when(userTradeService.verifyTrades(Mockito.anyString(), Mockito.anyList(), Mockito.anyString())).thenReturn(true);
@@ -114,10 +117,4 @@ public class ControllerTests {
         assertThat(expected).isEqualTo(result.getBody());
     }*/
 
-    // Add user end point (not required)
-    /*@Test
-    public void verifyTradesEndpointTest() throws Exception{
-        URI uri = new URI(baseUrl + "/addUser?userId=123");
-
-    } */
 }
--- Microservices/trading-service/src/test/java/io/tradingservice/tradingservice/serviceTests/ServiceTests.java
+++ Microservices/trading-service/src/test/java/io/tradingservice/tradingservice/serviceTests/ServiceTests.java
@@ -1,6 +1,7 @@
 package io.tradingservice.tradingservice.serviceTests;
 
 import io.tradingservice.tradingservice.models.ImmutableTrade;
+import io.tradingservice.tradingservice.models.Trade;
 import io.tradingservice.tradingservice.models.TradeParser;
 import io.tradingservice.tradingservice.repositories.UserAccessObject;
 import io.tradingservice.tradingservice.services.UserTradeService;
@@ -28,28 +29,31 @@ public class ServiceTests {
 
     ImmutableTrade trade;
     List<ImmutableTrade> trades;
-    TradeParser t;
+    //TradeParser t;
 
     @Test
-    public void successTest(){
-        assert 1==1;
+    public void successTest() {
+
+        assert 1 == 1;
     }
 
     @Before
     public void init() {
+
         MockitoAnnotations.initMocks(this);
     }
 
     @Before
-    public void setUp(){
-        //trade = ImmutableTrade.builder().fundName("Hedge").fundNumber("1234").build();
-        trade = ImmutableTrade.builder().fundNumber("1234").fundName("Hedge").avgNav((float)22)
-                .status("purchase").quantity((float)7).invManager("GS").setCycle(2).invCurr("INR")
-                .sAndPRating((float)23.2).moodysRating((float)12).build();
+    public void setUp() {
+
+        trade = ImmutableTrade.builder().fundNumber("1234").fundName("Hedge").avgNav((float) 22)
+                .status("purchase").quantity((float) 7).invManager("GS").setCycle(2).invCurr("INR")
+                .sAndPRating((float) 23.2).moodysRating((float) 12).build();
     }
 
     @Test
-    public void getAllTradesServiceTest(){
+    public void getAllTradesServiceTest() {
+
         List<ImmutableTrade> trades = new ArrayList<>();
         trades.add(trade);
 
@@ -58,50 +62,81 @@ public class ServiceTests {
     }
 
     @Test
-    public void addUserByIdServiceTest(){
+    public void addUserByIdServiceTest() {
+
         Mockito.when(userAccessObject.addUser(Mockito.any(String.class))).thenReturn(true);
         Assert.assertEquals(true, userTradeService.addUserById("123"));
     }
 
 //    @Test
 //    public void makeTradesServiceTest() {
+//
 //        List<ImmutableTrade> trades = new ArrayList<>();
 //
-//        trade = ImmutableTrade.builder().fundNumber("1").fundName("Hedge").avgNav((float) 17)
-//                .status("purchase").quantity((float) 9).invManager("GS").setCycle(4).invCurr("INR")
-//                .sAndPRating((float) 2.32).moodysRating((float) 13).build();
+//        TradeParser tradeParser = new TradeParser();
+//        tradeParser.setFundNumber("1234");
+//        tradeParser.setStatus("purchase");
+//        tradeParser.setQuantity(7);
+//
+//        Trade newtrade = ImmutableTrade.builder().fundNumber(tradeParser.getFundNumber()).fundName("Hedge").avgNav((float) 22)
+//                .status(tradeParser.getStatus()).quantity((float) tradeParser.getQuantity()).invManager("GS").setCycle(2).invCurr("INR")
+//                .sAndPRating((float) 23.2).moodysRating((float) 12).build();
+//
+//        Assert.assertEquals(newtrade, trade);
+//    }
+//
+//
+//    @Test
+//    public void verifyTradesServiceTest() {
+//        //verify(String userId, List<Trade> newTrades, float balance, String baseCurr)
+//        float balance=100.0f;
+//
+//
+//
+//    }
+//
+//    @Test
+//    public float exchangeTradeServiceTest() {
 //
 //        trades.add(trade);
 //
-//        Mockito.when(userAccessObject.addTrade(Mockito.anyString(), Mockito.any() , Mockito.anyFloat(), Mockito.anyString()))
+//        ImmutableTrade newtrade = ImmutableTrade.builder().fundNumber("1").fundName("Hedge").avgNav((float) 17)
+//                .status("purchase").quantity((float) 9).invManager("GS").setCycle(4).invCurr("INR")
+//                .sAndPRating((float) 2.32).moodysRating((float) 13).build();
+//
+//        trades.add(newtrade);
+//        for (ImmutableTrade t : trades) {
+//            // Fund already exists
+//            if (t.fundNumber().equals("1")) {
+//                //updateFund (String userId, Trade trade, String fundId, float balance, String baseCurr)
+//                userAccessObject.updateFund(Mockito.anyString(), Mockito.any(), Mockito.anyString(), Mockito.anyFloat(), Mockito.anyString());
+//            }
+//        }
+//        /*// Fund doesn't exist
+//        if (count==trades.size()){
+//            //directAddFund (String userId, Trade trade)
+//            userAccessObject.directAddFund(Mockito.anyString(), Mockito.any());
+//            float debit = trade.quantity()*trade.avgNav();
+//            return -debit;
+//        }*/
+//
+//        Mockito.when(userTradeService.verifyTrades(Mockito.anyString()))
 //                .thenReturn();
-//        Assert.assertEquals(200, trades.add(trade));
+//        Assert.assertEquals();
+//
+//        /*Mockito.when(userAccessObject.addTrade(Mockito.anyString(), Mockito.any() , Mockito.anyFloat(), Mockito.anyString()))
+//                .thenReturn();
+//        Assert.assertEquals(200, trades.add(trade).Response.status());*/
+//
 //    }
-
-    /*
-    @Test
-    public void verifyTradesServiceTest(){
-
-    }
-
-    @Test
-    public void exchangeTradeServiceTest(){
-        ImmutableTrade.Builder t = new TradeParser();
-
-        t = ImmutableTrade.builder().fundNumber("1").quantity((float)9).status("purchase");
-
-        trades.add((trade);
-
-        *//*Mockito.when(userAccessObject.addTrade(Mockito.anyList(Trade))).thenReturn(trades);
-        Assert.assertEquals(200, trades.add(trade).Response.status());*//*
-    }
-
-    @Test
-    public void updateUserTradeServiceTest(){
-        Optional<trades> optional = Optional.fromNullable(null);
-        Mockito.when(userTradeService.updateUser(Mockito.any(Trade.class))).thenReturn(optional);
-        *//*Assert.assertEquals("Fund with number " + fundDb.fundNumber() + " not found in db.",
-                service.update(fund).getMessage());*//*
-        Assert.assertEquals(404, userTradeService.updateUser().getStatus());
-    } */
+//
+//    /*
+//    @Test
+//    public void updateUserTradeServiceTest(){
+//        Optional<trades> optional = Optional.fromNullable(null);
+//        Mockito.when(userTradeService.updateUser(Mockito.any(Trade.class))).thenReturn(optional);
+//        *//*Assert.assertEquals("Fund with number " + fundDb.fundNumber() + " not found in db.",
+//                service.update(fund).getMessage());*//*
+//        Assert.assertEquals(404, userTradeService.updateUser().getStatus());
+//    } */
 }
